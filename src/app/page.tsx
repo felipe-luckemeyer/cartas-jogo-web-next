d4c5e20735f326a2c425cfe287b947e42c7e3059
Felipe <felipe.luckemeyer10@gmail.com>
2023-04-02 22:59:17 -0300

    Add HeroCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,32 @@
-import { Icons } from '@/assets'
-import Image from 'next/image'
+import { HeroCard } from '@/components'
 import Link from 'next/link'
 
-interface CardValue {
-  clan: string
-}
-
-const cardObj: CardValue = {
-  clan: 'magic',
-}
+const data = [
+  {
+    name: 'Mago',
+    clan: 'magic',
+    manaPoints: 2,
+    attackPoints: 3,
+    lifePoints: 5,
+  },
+  {
+    name: 'Dragão Negro',
+    clan: 'fire',
+    manaPoints: 10,
+    attackPoints: 10,
+    lifePoints: 15,
+  },
+]
 
 export default function Home() {
-  function getClanColor() {
-    switch (cardObj.clan) {
-      case 'strength':
-        return 'yellow-900'
-      case 'fire':
-        return 'amber-300'
-      case 'magic':
-        return 'purple-900'
-      default:
-        break
-    }
-  }
-  function getClanIcon() {
-    switch (cardObj.clan) {
-      case 'strength':
-        return Icons.shield
-      case 'fire':
-        return Icons.fire
-      case 'magic':
-        return Icons.magic
-      default:
-        break
-    }
-  }
-
   return (
     <div>
       <h1 className="text-3xl font-bold">Home</h1>
       <Link href="/battle">Battle</Link>
       <Link href="/collection">Collection</Link>
-      <div
-        className={`h-48 w-32 bg-${getClanColor()} p-1 rounded ml-80 shadow-custom01`}
-      >
-        <div className="h-4 w-24 bg-white rounded-sm flex absolute mt-2 ml-5 pl-1 shadow-custom01">
-          <label className="font-serif text-xs">Mago</label>
-        </div>
-        <div className="w-6 h-6 bg-cyan-600 rounded-full flex items-center justify-center border-2 border-white absolute mt-1 ml-0 shadow-custom01">
-          <label className="font-mono font-bold text-xs text-white">3</label>
-        </div>
-        <div className="w-28 h-10 bg-neutral-800 border-2 border-orange-300 rounded-sm absolute mt-32 ml-1 shadow-custom01"></div>
-        <div className="w-24 h-8 bg-neutral-800 border-2 border-orange-300 rounded-sm absolute mt-36 ml-3 shadow-custom01"></div>
-        <Image
-          src={Icons.profile}
-          alt="Texto alternativo"
-          width={90}
-          height={95}
-          className={
-            'bg-white rounded-full border-2 border-orange-300 scale-y-125 object-fill absolute mt-12 ml-[14px] shadow-custom01'
-          }
-          priority
-        />
-        <Image
-          src={getClanIcon()}
-          alt="Texto alternativo"
-          width={26}
-          height={26}
-          className={'absolute mt-36 ml-[46px]'}
-        />
-        <div className="w-6 h-6 bg-orange-400 rounded-full flex items-center justify-center absolute mt-32 ml-[22px] shadow-custom01">
-          <label className="font-mono font-bold text-white">2</label>
-        </div>
-        <div className="w-6 h-6 bg-red-500 rounded-full flex items-center justify-center absolute mt-32 mx-[73px] shadow-custom01">
-          <label className="font-mono font-bold text-white">6</label>
-        </div>
-      </div>
+      {data.map((item, index) => (
+        <HeroCard key={`${index}-${item.name}`} data={item} />
+      ))}
     </div>
   )
 }
